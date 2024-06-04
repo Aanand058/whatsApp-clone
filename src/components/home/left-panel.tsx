2da7309369765db2b48302ff7e5d28165137bc28
Aanand Aman <yamit058@gmail.com>
2024-06-04 17:21:12 -0400

    UI Completion

--- src/components/home/left-panel.tsx
+++ src/components/home/left-panel.tsx
@@ -6,7 +6,6 @@ import Conversation from "./conversation";
 
 const LeftPanel = () => {
 
-
   return (
     <div className='w-1/4 border-gray-600 border-r'>
       <div className='sticky top-0 bg-left-panel z-10'>
