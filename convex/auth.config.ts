adbd582bf2771e85fb3488be4a09feedce9bcd70
Aanand Aman <yamit058@gmail.com>
2024-06-10 23:04:09 -0400

    Auth and WebHooks Added

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig = {
+    providers: [
+        {
+            domain: "https://cheerful-pika-34.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig;
--- convex/tasks.ts
+++
@@ -1,9 +0,0 @@
-import { query, mutation } from "./_generated/server";
-
-export const getTasks = query({
-    args: {},
-    handler: async (ctx, args) => {
-       const tasks=  await ctx.db.query("tasks").collect()
-    return tasks;
-    }
-})
